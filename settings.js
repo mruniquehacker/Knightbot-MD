6780100b873b80491a8f09b56f2815a98be2b4be
Peywa <120773723+Peywa@users.noreply.github.com>
2025-02-04 16:57:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  author: 'MR PEYWA',
+  botName: "CODE_IT BOT",
+  botOwner: 'MR PEYWA', // Your name
+  ownerNumber: '254792936892', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
