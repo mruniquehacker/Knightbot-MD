a261447fbd9071301b6743aa94641c3c63c3ab22
Peywa <120773723+Peywa@users.noreply.github.com>
2025-02-05 16:27:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'MR PEYWA',
   botName: "CODE_IT BOT",
   botOwner: 'MR PEYWA', // Your name
-  ownerNumber: '254792936892', //Your number
+  ownerNumber: '13305972014', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
