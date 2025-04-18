bc315f249115c665ce6d27fbb788f27b6f116023
macoder67 <manyakaontiretse@gmail.com>
2025-04-18 18:29:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
+  botName: "macoder67",
+  botOwner: 'VIBRA KEYZ', // Your name
   ownerNumber: '27656136438', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
