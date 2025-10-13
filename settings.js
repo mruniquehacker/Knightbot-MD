75167424854c6cb88f1c1718e51e06e7132c301e
tanmoykumar428-byte <tanmoykumar428@gmail.com>
2025-10-13 11:41:18 +0530

    Update settings.js
    ['.mute', '.unmute', '.ban', '.unban', '.promote', '.demote', '.kick', '.tagall', '.tagnotadmin', '.hidetag', '.antilink', '.antitag', '.setgdesc', '.setgname', '.setgpp'];

--- settings.js
+++ settings.js
@@ -1,16 +1,17 @@
-const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+const settings = {['.mute', '.unmute', '.ban', '.unban', '.promote', '.demote', '.kick', '.tagall', '.tagnotadmin', '.hidetag', '.antilink', '.antitag', '.setgdesc', '.setgname', '.setgpp'];
+  packname: 'lord alex Bot',
+  author: '‎',xeon bot
+  botName: "lord aoex bot",
+  botOwner: 'Professor', /lord alex/
+  ownerNumber: '+91 96781 31589', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.0",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mr lord alex/lord alex-MD/archive/refs/heads/main.zip",
 };
-
-module.exports = settings;
+commend onlywoner
+module.exports = settings; https://['.mute', '.unmute', '.ban', '.unban', '.promote', '.demote', '.kick', '.tagall', '.tagnotadmin', '.hidetag', '.antilink', '.antitag', '.setgdesc', '.setgname', '.setgpp'];github.com/
+https: lord alex
