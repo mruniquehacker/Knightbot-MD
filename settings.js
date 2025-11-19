20b3889153a58d9b8705347d1eb90a95624f1219
Har404-err <abdulmuhar564@gmail.com>
2025-11-19 14:51:51 +0000

    Setting update

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Jarvis',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Jarvis MD",
+  botOwner: 'Har', // Your name
+  ownerNumber: '6282148570591', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
